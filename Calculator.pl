efe3aa14aaed5e4dcc514c6559d56192b7205839
unknown <cooltime15@likelion.org>
2017-11-27 19:36:24 +0900

    quest complete

--- Calculator.pl
+++ Calculator.pl
@@ -2,12 +2,31 @@
 $initValue = 0;
 
 print "== Yoon SuHyeok Calculator ==\n";
+print "$initValue";
 
 while(true){
    $in = <STDIN>;
    
-   if ( $in == "q" ){
-      print "HI";
+   @array = split(" ", $in);
+
+   if( $array[0] eq "+" ) {
+      $initValue = ($initValue + $array[1]);
+      print "$initValue";
+   }
+   elsif( $array[0] eq "-" ) {
+      $initValue = ($initValue - $array[1]);
+      print "$initValue";
+   }
+   elsif( $array[0] eq "/" ) {
+      $initValue = ($initValue / $array[1]);
+      print "$initValue";
+   }
+   elsif( $array[0] eq "*" ) {
+      $initValue = ($initValue * $array[1]);
+      print "$initValue";
+   } 
+   elsif ( $in == "q" ) {
+      exit;
    }
   
 }
